d255b772e4619fea177fac6c2d947af0b363309f
vickydarlinn <uttapalsangwan@gmail.com>
2025-05-27 18:33:52 +0530

    feat:implement drawer

--- src/navigation/DrawerNavigator.tsx
+++ src/navigation/DrawerNavigator.tsx
@@ -1,23 +1,23 @@
-// import React from 'react';
-// import {createDrawerNavigator} from '@react-navigation/drawer';
-// import BottomTabNavigator from './BottomTabNavigator';
-// import Settings from '../screens/Settings';
-// import Login from '../screens/Login';
+import React from 'react';
+import {createDrawerNavigator} from '@react-navigation/drawer';
+import BottomTabNavigator from './BottomTabNavigator';
+import Settings from '../screens/Settings';
+import Login from '../screens/Login';
 
-// const Drawer = createDrawerNavigator();
+const Drawer = createDrawerNavigator();
 
-// const DrawerNavigator = () => {
-//   return (
-//     <Drawer.Navigator initialRouteName="Main">
-//       <Drawer.Screen
-//         name="Main"
-//         component={BottomTabNavigator}
-//         options={{headerShown: false}}
-//       />
-//       <Drawer.Screen name="Settings" component={Settings} />
-//       <Drawer.Screen name="Login" component={Login} />
-//     </Drawer.Navigator>
-//   );
-// };
+const DrawerNavigator = () => {
+  return (
+    <Drawer.Navigator initialRouteName="Main">
+      <Drawer.Screen
+        name="Main"
+        component={BottomTabNavigator}
+        options={{headerShown: false}}
+      />
+      <Drawer.Screen name="Settings" component={Settings} />
+      <Drawer.Screen name="Login" component={Login} />
+    </Drawer.Navigator>
+  );
+};
 
-// export default DrawerNavigator;
+export default DrawerNavigator;
--- src/providers/AppProvider.tsx
+++ src/providers/AppProvider.tsx
@@ -1,15 +1,16 @@
 import {Provider as ReduxProvider} from 'react-redux';
 import {NavigationContainer} from '@react-navigation/native';
 import {store} from '../store';
-import BottomTabNavigator from '../navigation/BottomTabNavigator';
+
 import {SafeAreaProvider} from 'react-native-safe-area-context';
+import DrawerNavigator from '../navigation/DrawerNavigator';
 
 const AppProvider = () => {
   return (
     <SafeAreaProvider>
       <ReduxProvider store={store}>
         <NavigationContainer>
-          <BottomTabNavigator />
+          <DrawerNavigator />
         </NavigationContainer>
       </ReduxProvider>
     </SafeAreaProvider>
